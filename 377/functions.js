b4261da0f7ab1b4642509765cde8e6e6931c96c4
Cunningham <cunsco@f01898096c90.ant.amazon.com>
2021-07-30 13:32:42 -0600

    Fri Jul 30 19:32:42 UTC 2021

---
+++ 377/functions.js
@@ -0,0 +1,493 @@
+//
+
+const apiendpointurl = "https://jl66fjfil5.execute-api.us-west-2.amazonaws.com/eng/playout"
+
+//// LEAVE CODE BELOW THIS LINE /////
+
+function getConfig(){
+  var json_data,
+  current_url = window.location.href
+  json_endpoint = current_url.substring(0,current_url.lastIndexOf("/")) + "/channel_map.json"
+
+  var request = new XMLHttpRequest();
+  request.open('GET', json_endpoint, false);
+
+  request.onload = function() {
+
+  if (request.status === 200) {
+    const jdata = JSON.parse(request.responseText);
+    console.log(jdata)
+    window.live_event_map = jdata.channel_map
+    window.channel_start_slate = jdata.channel_start_slate
+    window.deployment_name = jdata.dashboard_title
+    window.bucket = jdata.vod_bucket
+    window.promo_bucket_region = jdata.promo_bucket_region
+    json_data = request.responseText
+     } else {
+    // Reached the server, but it returned an error
+  }
+}
+
+request.onerror = function() {
+  console.error('An error occurred fetching the JSON from ' + json_endpoint);
+};
+
+request.send();
+return json_data
+} // end
+
+function mediaLiveControl(evt, controlName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(controlName).style.display = "block";
+  evt.currentTarget.className += " active";
+
+  // livestatic vodslate
+  if ( controlName == "livestatic" ) {
+    if ( pipSelector != "" ) {
+      console.log("Selected tab is " + controlName + " need to run function to get available inputs/sources ")
+      getLiveInputs(apiendpointurl)
+    }
+  } else if ( controlName == "vodslate" ) {
+    console.log("Selected tab is " + controlName + " need to run function to get available inputs/sources ")
+    // s3 api call
+    s3getObjectsAPI(bucket, apiendpointurl)
+  } else {
+    console.log("Selected tab is " + controlName)
+  }
+}
+
+function chstartstopcontrol(action_type){
+  console.log("Running Channel Start/Stop function")
+  if ( pipSelector == "" ) {
+    console.log("Operator has not selected a channel thumbnail. Select a thumbnail first before an action can be performed")
+    alert("Please select a channel thumbnail first!")
+  } else {
+  document.getElementById(action_type).classList.add('pressedbutton');
+  console.log("action type: "+action_type+" for channel ID : "+live_event_map[pipSelector][0])
+  // API Call to start/stop channel
+  channelStartStop(action_type)
+
+  // reset styling on the pip now that the action has been performed
+  fadeAway(action_type)
+
+  }
+}
+
+function chliveswitch() {
+  console.log("Running Live input switch function")
+  if ( pipSelector == "" ) {
+    console.log("Operator has not selected a channel thumbnail. Select a thumbnail first before an action can be performed")
+    alert("Please select a channel thumbnail first!")
+  } else {
+  document.getElementById('live').classList.add('pressedbutton');
+  input = document.getElementById("live_source_dropdown_select").value
+  console.log("Switching to input: "+input+" for channel ID : "+live_event_map[pipSelector][0])
+  channelid = live_event_map[pipSelector][0] + ":" + live_event_map[pipSelector][2]
+  emlSwitchAction(input, channelid, "", "immediateSwitchLive", "", 200, "master", "immediateswitch")
+  }
+
+  // reset styling on the pip now that the action has been performed
+  fadeAway('live')
+}
+
+function chvodswitch(){
+  console.log("Running VOD input switch function")
+
+  if ( pipSelector == "" ) {
+    console.log("Operator has not selected a channel thumbnail. Select a thumbnail first before an action can be performed")
+    alert("Please select a channel thumbnail first!")
+  } else {
+  document.getElementById('vod').classList.add('pressedbutton');
+  input = document.getElementById("vod_source_dropdown_select").value
+  console.log("Switching to input: "+input+" for channel ID : "+live_event_map[pipSelector][0])
+  channelid = live_event_map[pipSelector][0] + ":" + live_event_map[pipSelector][2]
+  emlSwitchAction(input, channelid, bucket, "immediateSwitch", "", 200, "master", "immediateswitch")
+  }
+
+  fadeAway('vod')
+
+}
+
+function chpromoins(promo_number){
+  console.log("Running promo insert function")
+
+  if ( pipSelector == "" ) {
+    console.log("Operator has not selected a channel thumbnail. Select a thumbnail first before an action can be performed")
+    alert("Please select a channel thumbnail first!")
+  } else {
+    console.log("Selected Channel ID : "+live_event_map[pipSelector][0])
+    console.log("Found " + live_event_map[pipSelector][5].length + " promos in channnel map")
+
+    if ( promo_number > live_event_map[pipSelector][5].length ) {
+      alert("Cannot play promo, it doesn't exist. You need to update the channel map via API with the promo URL")
+
+    } else {
+    document.getElementById('promo'+promo_number).classList.add('pressedbutton');
+    promo_to_play = live_event_map[pipSelector][5][promo_number-1]
+    console.log("Sending an API call to MediaLive to start promo : " + promo_to_play)
+    s3_url = new URL(promo_to_play.replace("s3://","https://"))
+    bucket = s3_url.hostname
+    input = s3_url.pathname.replace(/^\/+/, '')
+    console.log("Promo bucket : " + bucket)
+    console.log("Promo key : " + input)
+
+    channelid = live_event_map[pipSelector][0] + ":" + live_event_map[pipSelector][2]
+    console.log("Submitting API Call to insert promo now")
+    emlSwitchAction(input, channelid, bucket, "immediateContinue", "", 200, "master", "")
+    }
+    // reset styling on the pip now that the action has been performed
+    fadeAway('promo'+promo_number)
+}
+
+} // end chpromoins function
+
+var fadeAway = function(buttonid) {
+  setTimeout(function(){
+  document.getElementById(buttonid).classList.remove('pressedbutton');
+ }, 2000);
+}
+
+function channelDropdownPopulate(){
+
+  let dropdown = document.getElementById('channel_selector');
+  dropdown.length = 0;
+
+  let defaultOption = document.createElement('option');
+  defaultOption.text = 'Select Channel';
+
+  dropdown.add(defaultOption);
+  dropdown.selectedIndex = 0;
+
+  for (channel in live_event_map){
+    //live_event_map[channel][3]
+    option = document.createElement('option');
+    option.text = live_event_map[channel][1];
+    option.value = channel;
+    dropdown.add(option)
+  }
+
+}
+
+var sldpPlayers = [];
+document.getElementById("channel_selector").onclick = function () {
+
+  // log selected channel number and set to pipSelector variable
+  pipSelector = document.getElementById("channel_selector").value
+  console.log("Channel " + pipSelector + " has been selected from the dropdown menu")
+
+  if ( pipSelector != "" ) {
+    channelState(pipSelector)
+  }
+
+  // Print channel information to channel info box
+  // id to populate = channel_info
+  document.getElementById("channel_info").innerHTML = '<p> Channel Name : '+live_event_map[pipSelector][1]+' </p>'
+  document.getElementById("channel_info").innerHTML += '<p> Channel ID : '+live_event_map[pipSelector][0]+' </p>'
+  document.getElementById("channel_info").innerHTML += '<p> AWS Region : '+live_event_map[pipSelector][2]+' </p>'
+
+  document.getElementById("channel_info").innerHTML += '<h3> Promo Videos </h3>'
+
+  for (s3_promo in live_event_map[pipSelector][5]){
+    s3_promo_url = new URL(live_event_map[pipSelector][5][s3_promo].replace("s3://","https://"))
+    promo_bucket = s3_promo_url.hostname
+    promo_key = s3_promo_url.pathname.replace(/^\/+/, '')
+    s3_https_url = 'https://'+promo_bucket+'.s3-'+promo_bucket_region+'.amazonaws.com/'+promo_key
+
+    document.getElementById("channel_info").innerHTML += '<a href="'+s3_https_url+'" target="_blank"> Promo : '+ promo_key +'</a></br>'
+  }
+
+
+  // initialize the low latency players
+  function startPlayers () {
+        console.log("starting sdlp players, checking to see if they are loaded already....")
+
+        if ( sldpPlayers.length > 0 ) {
+          console.log("players were already loaded, sending to restart function")
+          restartPlayers();
+        } else {
+          console.log("players not initialized, doing now....")
+          doStart();
+        }
+      }
+
+      function restartPlayers () {
+        var destroyCnt = 0;
+        for (var i = 0; i < sldpPlayers.length; i++) {
+          sldpPlayers[i].destroy(function () {
+            console.log("destroying current instance of player")
+            destroyCnt++;
+            if (destroyCnt === sldpPlayers.length) {
+              sldpPlayers = [];
+              doStart();
+            }
+          });
+        }
+      console.log("old instances of players have been removed.")
+      }
+
+      function doStart () {
+        console.log("initializing new sdlp players")
+        for (var i = 0; i < 2; i++) {
+          var player = SLDP.init({
+            container:          'player-wrp-' + (i + 1),
+            stream_url:         live_event_map[pipSelector][3+i],
+            buffering:          500,
+            autoplay:           true,
+            muted:              true,
+            height:             360,
+            width:              640,
+            vu_meter:           {type: 'input', mode: 'peak', container: 'vu-meter-' + (i + 1), rate: 10},
+          });
+          sldpPlayers[i] = player;
+        }
+      console.log("done initializing low latency players.")
+      }
+  startPlayers()
+}
+
+
+
+function pageLoadFunction(){
+  getConfig()
+  console.log("channel map : " + JSON.stringify(live_event_map))
+  console.log("channel start slate: "+ channel_start_slate)
+  console.log("vod bucket: " + bucket)
+  console.log("dashboard name: " + deployment_name)
+  var s3_slate_url = new URL(channel_start_slate.replace("s3://","https://"))
+  window.slate_bucket = s3_slate_url.hostname
+  window.startup_slate_key = s3_slate_url.pathname.replace(/^\/+/, '')
+
+  // write deployment title
+  var deployment_name_pretty = deployment_name.toUpperCase().replace("_"," ")
+  document.getElementById('deployment_title').innerHTML = '<h1 style="text-align:center">'+deployment_name_pretty+'</h1>'
+
+  var total_channels = Object.keys(live_event_map).length;
+console.log("There are " + total_channels.toString() + " channels in this dashboard")
+window.thumbnail_size = 1
+if ( parseInt(total_channels) < 9 ) {
+  window.thumbnail_size = 2
+} else {
+  window.thumbnail_size = 1
+}
+
+  channelDropdownPopulate()
+
+  pipSelector = ""
+
+}
+
+setInterval(function() {
+  channelState()
+}, 5000);
+
+/// API Calls
+/// S3 GET OBJECT API CALL - START
+
+function channelState() {
+    console.log("channel state api call: initializing")
+    var channellist = [];
+    var channelid = live_event_map[pipSelector][0]  + ":" + live_event_map[pipSelector][2]
+
+    var param1 = "awsaccount=master";
+    var param2 = "&functiontorun=describeChannelState"
+    var param3 = "&channelid="+channelid; // this needs to be full list of channel id's and regions
+    var param4 = "&maxresults=200";
+    var param5 = "&bucket=";
+    var param6 = "&input=";
+    var param7 = "&follow=";
+    var url = apiendpointurl+"?"+param1+param2+param3+param4+param5+param6+param7
+
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    request.onload = function() {
+      if (request.status === 200) {
+        const state_data = JSON.parse(request.responseText);
+        console.log("channel state api call response : " + state_data)
+        document.getElementById('channel_status').innerHTML = '<h3>Channel Status:</br>'+state_data.status+'</h3>'
+       } else {
+         error_message = "Unable to get channel status"
+         document.getElementById('channel_status').innerHTML = '<h3>Channel Status:</br>'+error_message+'</h3>'
+        // Reached the server, but it returned an error
+      }
+    }
+    request.onerror = function() {
+      console.error('An error occurred fetching the JSON from ' + url);
+    };
+
+    request.send();
+}
+
+function s3getObjectsAPI(bucket, apiendpointurl) {
+    console.log("s3 get objects api call: initializing")
+    var param1 = "awsaccount=master";
+    var param2 = "&functiontorun=s3GetAssetList"
+    var param3 = "&channelid=0:x";
+    var param4 = "&maxresults=200";
+    var param5 = "&bucket="+bucket;
+    var param6 = "&input=";
+    var param7 = "&follow=";
+    var url = apiendpointurl+"?"+param1+param2+param3+param4+param5+param6+param7
+    console.log("Executing API Call to get S3 assets: " +url )
+
+    let dropdown = document.getElementById('vod_source_dropdown_select');
+    dropdown.length = 0;
+
+    let defaultOption = document.createElement('option');
+    defaultOption.text = 'Choose Asset';
+
+    dropdown.add(defaultOption);
+    dropdown.selectedIndex = 0;
+
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    request.onload = function() {
+      if (request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        console.log("s3 get objects api call response: " + data)
+        let option;
+        for (let i = 0; i < data.length; i++) {
+          option = document.createElement('option');
+          option.text = data[i].name;
+          option.value = data[i].key;
+          dropdown.add(option);
+        }
+       } else {
+        // Reached the server, but it returned an error
+      }
+    }
+
+    request.onerror = function() {
+      console.error('An error occurred fetching the JSON from ' + url);
+    };
+
+    request.send();
+}
+
+/// S3 GET OBJECT API CALL - END
+///
+/// EML GET ATTACHED INPUTS - START
+
+function getLiveInputs(apiendpointurl) {
+    console.log("get live inputs api call: initializing")
+    var channelid = live_event_map[pipSelector][0] + ":" + live_event_map[pipSelector][2];
+    var input = document.getElementById("live_source_dropdown_select").value;
+
+    var param1 = "awsaccount=master";
+    var param2 = "&functiontorun=getAttachedInputs"
+    var param3 = "&channelid="+channelid;
+    var param4 = "&maxresults=200";
+    var param5 = "&bucket=";
+    var param6 = "&input="+input;
+    var param7 = "&follow=";
+
+    var url = apiendpointurl+"?"+param1+param2+param3+param4+param5+param6+param7
+    console.log("Executing API call to get attached inputs to MediaLive Channel " + channelid )
+
+    let dropdown = document.getElementById('live_source_dropdown_select');
+    dropdown.length = 0;
+
+    let defaultOption = document.createElement('option');
+    defaultOption.text = 'Choose Live Source';
+
+    dropdown.add(defaultOption);
+    dropdown.selectedIndex = 0;
+
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    request.onload = function() {
+      if (request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        console.log("get live inputs api call response : " + data)
+        let option;
+        for (let i = 0; i < data.length; i++) {
+          option = document.createElement('option');
+          option.text = data[i].name;
+          option.value = data[i].name;
+          dropdown.add(option);
+        }
+       } else {
+        // Reached the server, but it returned an error
+      }
+    }
+    request.onerror = function() {
+      console.error('An error occurred fetching the JSON from ' + url);
+    };
+
+    request.send();
+}
+
+/// EML GET ATTACHED INPUTS - END
+
+/// EML SWITCH - START
+
+function emlSwitchAction(file, channelid, bucket, takeType, follow, maxresults, awsaccount, scte){
+    console.log("eml switch action api call: initializing")
+    console.log("Executing API PUT action for switch type "+takeType)
+
+    var param1 = "awsaccount="+awsaccount;
+    var param2 = "&functiontorun="+takeType
+    var param3 = "&channelid="+channelid;
+    var param4 = "&maxresults="+maxresults;
+    var param5 = "&bucket="+bucket;
+    var param6 = "&input="+file;
+    var param7 = "&follow="+follow;
+    var param8 = "&duration="+scte;
+    var url = apiendpointurl+"?"+param1+param2+param3+param4+param5+param6+param7+param8
+    console.log("eml switch action api call - executing : "+url)
+
+    var putReq = new XMLHttpRequest();
+    putReq.open("PUT", url, false);
+    putReq.setRequestHeader("Accept","*/*");
+    putReq.send();
+}
+
+/// EML SWITCH - END
+
+/// EML SWITCH VOD - START
+
+/// EML SWITCH VOD - END
+
+/// EML CHANNEL START/STOP - START
+
+function channelStartStop(startstop){
+
+    if (pipSelector.length < 1){
+      alert("Select a channel first...");
+      return;
+    }
+
+    console.log("channel start-stop action api call: initializing")
+    channelid = live_event_map[pipSelector][0] + ":" + live_event_map[pipSelector][2];
+
+    var param1 = "awsaccount=master";
+    var param2 = "&functiontorun=channelStartStop"
+    var param3 = "&channelid="+channelid;
+    var param4 = "&maxresults=200";
+    var param5 = "&bucket="+slate_bucket + ":" + startup_slate_key.replace(/\//g,"%2F");
+    var param6 = "&input="+startstop;
+    var param7 = "&follow=";
+    var param8 = "&duration=";
+    var url = apiendpointurl+"?"+param1+param2+param3+param4+param5+param6+param7+param8
+    console.log("channel start-stop action api call - executing : " + channelid)
+
+    var putReq = new XMLHttpRequest();
+    putReq.open("PUT", url, false);
+    putReq.setRequestHeader("Accept","*/*");
+    putReq.send();
+
+    alert("Channel state is changing, please be patient. This may take 60-90 seconds")
+}
+
+/// EML CHANNEL START/STOP - END
+///
