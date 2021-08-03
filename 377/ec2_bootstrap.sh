d15c41cbd4efcd80ee746e004fcf3ec84485f113
Cunningham <cunsco@f01898096c90.ant.amazon.com>
2021-08-03 09:38:18 -0600

    Tue Aug  3 15:38:18 UTC 2021

--- 377/ec2_bootstrap.sh
+++ 377/ec2_bootstrap.sh
@@ -3,8 +3,7 @@ sudo yum -y install httpd
 sudo yum -y install mod_ssl
 sudo mkdir /etc/ssl/private
 sudo chmod 700 /etc/ssl/private
-openssl req -new -newkey rsa:4096 -days 0 -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" -keyout /etc/ssl/private/streamer.key  -out /etc/ssl/private/streamer.cert
-
+openssl req -new -newkey rsa:4096 -days 0 -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.streamer.com" -keyout /etc/ssl/private/streamer.key  -out /etc/ssl/private/streamer.cert
 sudo bash -c 'echo -e "[nimble]\nname= Nimble Streamer repository\nbaseurl=http://nimblestreamer.com/centos/7/\$basearch\nenabled=1\ngpgcheck=1\ngpgkey=http://nimblestreamer.com/gpg.key\n" > /etc/yum.repos.d/nimble.repo'
 sudo yum -y makecache
 sudo yum -y install nimble
