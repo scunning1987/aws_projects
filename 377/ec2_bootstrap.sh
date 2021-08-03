5b18e03dae9057d4277d0ff28fe5e92872190a8b
Cunningham <cunsco@f01898096c90.ant.amazon.com>
2021-08-03 08:33:50 -0600

    Tue Aug  3 14:33:50 UTC 2021

--- 377/ec2_bootstrap.sh
+++ 377/ec2_bootstrap.sh
@@ -2,5 +2,7 @@
 sudo bash -c 'echo -e "[nimble]\nname= Nimble Streamer repository\nbaseurl=http://nimblestreamer.com/centos/7/\$basearch\nenabled=1\ngpgcheck=1\ngpgkey=http://nimblestreamer.com/gpg.key\n" > /etc/yum.repos.d/nimble.repo'
 sudo yum -y makecache
 sudo yum -y install nimble
+sudo yum -y install wget
+sudo wget
 sudo python ~/ec2_stream_builder.py 40 20001
 sudo service nimble start
